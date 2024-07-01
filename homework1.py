5446cab93ce2114b5b034bcfae508e2627ad8660
pola0noch <klayriddell@gmail.com>
2024-07-01 16:04:46 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,14 @@
+# урок "Строки и индексация строк"
+example = "Топинамбур"
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
+
+example = "Помидор"
+print(example[0])
+print(example[-1])
+print(example[4:])
+print(example[::-1])
+print(example[1::2])
